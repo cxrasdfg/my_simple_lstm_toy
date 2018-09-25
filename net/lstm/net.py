c0d6938c672741d54fe6d8955a0855957faac7f2
theppsh <734331254@qq.com>
2018-09-26 00:27:59 +0800

    ee..

--- net/lstm/net.py
+++ net/lstm/net.py
@@ -9,8 +9,11 @@ class LSTM(th.nn.Module):
             voc_size (int): size of the vocabulary
         """
         super(LSTM,self).__init__()
-        self.W_f=th.nn.Parameter(torch.randn((voc_size+hidden_units),voc_size) )
-        self.b_f=th.nn.Parameter(torch.randn(voc_size))
+        self.W_f=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size) )
+        self.b_f=th.nn.Parameter(th.randn(voc_size))
+        self.W_i=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
+        self.b_i=th.nn.Parameter(th.randn(voc_size))
+        
 
     def forward(self,*args):
         if self.training:
