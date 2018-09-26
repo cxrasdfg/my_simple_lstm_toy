ad9860caea4c34790b6c02e2118661f958043cb2
theppsh <theppsh@example.com>
2018-09-26 10:08:26 +0800

    init

--- net/lstm/net.py
+++ net/lstm/net.py
@@ -9,12 +9,21 @@ class LSTM(th.nn.Module):
             voc_size (int): size of the vocabulary
         """
         super(LSTM,self).__init__()
+
         self.W_f=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size) )
         self.b_f=th.nn.Parameter(th.randn(voc_size))
+        
         self.W_i=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
         self.b_i=th.nn.Parameter(th.randn(voc_size))
         
+        self.W_c=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
+        self.b_c=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
+        
+        self.W_o=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
+        self.b_o=th.nn.Parameter(th.rannd((voc_size+hidden_units),voc_size))
 
+        
+        
     def forward(self,*args):
         if self.training:
             X,Y=args
