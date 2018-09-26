17306a37dc3d3fdbc54428aabb90fb0716a2700e
theppsh <theppsh@example.com>
2018-09-26 10:13:53 +0800

    modify it

--- net/lstm/net.py
+++ net/lstm/net.py
@@ -10,23 +10,20 @@ class LSTM(th.nn.Module):
         """
         super(LSTM,self).__init__()
 
-        self.W_f=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size) )
-        self.b_f=th.nn.Parameter(th.randn(voc_size))
+        self.W_f=th.nn.Parameter(th.randn((voc_size+1+hidden_units),hidden_units) )
+        self.b_f=th.nn.Parameter(th.randn(hidden_units))
         
-        self.W_i=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
-        self.b_i=th.nn.Parameter(th.randn(voc_size))
+        self.W_i=th.nn.Parameter(th.randn((voc_size+1+hidden_units),hidden_units))
+        self.b_i=th.nn.Parameter(th.randn(hidden_units))
         
-        self.W_c=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
-        self.b_c=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
+        self.W_c=th.nn.Parameter(th.randn((voc_size+1+hidden_units),hidden_units))
+        self.b_c=th.nn.Parameter(th.randn(hidden_units)
         
-        self.W_o=th.nn.Parameter(th.randn((voc_size+hidden_units),voc_size))
-        self.b_o=th.nn.Parameter(th.rannd((voc_size+hidden_units),voc_size))
-
-        git
+        self.W_o=th.nn.Parameter(th.randn((voc_size+1+hidden_units),hidden_units))
+        self.b_o=th.nn.Parameter(th.rannd(hidden_units)
         
     def forward(self,*args):
         if self.training:
-            X,Y=args
-            
+            X,Y=args # [b,seq_len,voc_size+1] 
         else:
             raise NotImplementedError()
